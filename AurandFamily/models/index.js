97a98c1ad10462b9a9db39b73c0bc52946958fe8
Jamie Aurand <jamie@cardtapp.com>
2015-02-15 12:20:47 -0800

    try this connection

--- AurandFamily/models/index.js
+++ AurandFamily/models/index.js
@@ -8,15 +8,9 @@
     
     if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
         // the application is executed on Heroku ... use the postgres database
-        var match = process.env.HEROKU_POSTGRESQL_VIOLET_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+        //var match = process.env.HEROKU_POSTGRESQL_VIOLET_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
         
-        sequelize = new Sequelize(match[5], match[1], match[2], {
-            dialect: 'postgres',
-            protocol: 'postgres',
-            port: match[4],
-            host: match[3],
-            logging: true //false
-        });
+        sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_VIOLET_URL);
     } else {
         // the application is executed on the local machine ... use mysql
         sequelize = new Sequelize('aurand', 'postgres', 'password', { host: 'localhost', dialect: 'postgres' });
